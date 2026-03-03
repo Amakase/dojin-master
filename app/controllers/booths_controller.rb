73e56409892eb12a2647f8be5f464cc4a4749c47
Jonathan Kiichi Kimura <picard33470@gmail.com>
2026-03-03 17:45:09 +0900

    Created skeleton of show page for booths

--- app/controllers/booths_controller.rb
+++ app/controllers/booths_controller.rb
@@ -1,2 +1,10 @@
 class BoothsController < ApplicationController
+  def show
+    @booth = Booth.find(params[:id])
+    @event = @booth.event
+    @notifications = @booth.notifications
+    @booth_works = @booth.booth_works
+    @user_inventory = current_user.collections.first.collection_works
+    # raise
+  end
 end
