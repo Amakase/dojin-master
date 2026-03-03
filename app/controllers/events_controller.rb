3fbd33cb94142f1670923de99a58d35f755590a9
steven <stevencheng926@gmail.com>
2026-03-03 17:53:51 +0900

    Events & event List,controller+view+css

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,10 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find_by(id: params[:id])
+    @booths = @event.booths.where(params[:event_id])
+  end
 end
