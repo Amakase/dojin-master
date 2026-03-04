9a9bb0969af442eec515e820217108da2a2dec7f
steven <stevencheng926@gmail.com>
2026-03-04 11:55:58 +0900

    update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,10 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+  include Pundit::Authorization
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
   def index
     if params[:filter] == "bookmarked"
       @events = current_user.events
@@ -10,5 +16,11 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     @booths = @event.booths
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
