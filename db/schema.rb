02e7cc771f9962509e1da73a214a53de4c64964a
Steven Sawadisavi <stevesavi+lewagon@gmail.com>
2026-03-04 12:41:08 +0900

    uncommented validations related to the db defaults change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_04_032600) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_04_033913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -181,7 +181,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_04_032600) do
     t.string "title"
     t.string "title_reading"
     t.datetime "updated_at", null: false
-    t.string "version"
+    t.string "version", default: ""
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
