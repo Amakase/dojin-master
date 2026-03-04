64d929f24078c4ccd401f79c70b5a64ccbe2704b
Jonathan Kiichi Kimura <picard33470@gmail.com>
2026-03-04 14:12:27 +0900

    changed booths_controller.rb and _user_inventory.html.erb for efficiency

--- app/controllers/booths_controller.rb
+++ app/controllers/booths_controller.rb
@@ -1,13 +1,12 @@
 class BoothsController < ApplicationController
-  def index; end
-
   def show
     @booth = Booth.find(params[:id])
-    authorize @booth
-
     @event = @booth.event
     @notifications = @booth.notifications
     @booth_works = @booth.booth_works
     @user_inventory = current_user.collections.first.collection_works
+                                  .joins(work: :circle_works)
+                                  .where(circle_works: { circle_id: @booth.circle_id })
+                                  .includes(:work)
   end
 end
