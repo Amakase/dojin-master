fa4fa82910388d451d59f1bb4f6812e58d38e61c
steven <stevencheng926@gmail.com>
2026-03-04 13:27:06 +0900

    event-policy

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,27 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+  include Pundit::Authorization
+
+  # Pundit: allow-list approach
+  after_action :verify_authorized, except: %i[index], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: %i[index], unless: :skip_pundit?
   def index
     if params[:filter] == "bookmarked"
-      @events = current_user.events
+      @events = policy_scope(current_user.events)
     else
-      @events = Event.all
+      @events = policy_scope(Event)
     end
   end
 
   def show
-    @event = Event.find(params[:id])
+    @event = policy_scope(Event).find(params[:id])
     @booths = @event.booths
+    authorize @event, :show?
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
