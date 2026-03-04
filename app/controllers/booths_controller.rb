52c918d95c937df7b3ab975f0ef9fd5676999f77
Jonathan Kiichi Kimura <picard33470@gmail.com>
2026-03-04 14:11:07 +0900

    Changes to booth controller and user_inventory partial

--- app/controllers/booths_controller.rb
+++ app/controllers/booths_controller.rb
@@ -5,5 +5,8 @@ class BoothsController < ApplicationController
     @notifications = @booth.notifications
     @booth_works = @booth.booth_works
     @user_inventory = current_user.collections.first.collection_works
+                                  .joins(work: :circle_works)
+                                  .where(circle_works: { circle_id: @booth.circle_id })
+                                  .includes(:work)
   end
 end
